afbd9c282fd52d138da6653f5062bc4b9340bb10
beasb <bia4sb4@gmail.com>
2025-01-10 13:03:11 -0300

    atividade blog pessoal

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,62 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity                             // indica que a a classe é uma tabela
+@Table(name = "tb_postagens")       // indica o nome da tabela no banco de dados
+public class Postagem {
+
+	@Id                                                              // ele é a chave primaria
+	@GeneratedValue(strategy = GenerationType.IDENTITY)              // auto increment
+	private Long id;
+	
+	@NotBlank(message = "Esse campo é pbrigatório!")      // Not Null
+	@Size(min = 5, max = 100, message = "Digite no mínimo 5 e no máximo 100 caracteres")
+	private String titulo;
+	
+	@NotBlank(message = "Esse campo é pbrigatório!")    
+	@Size(min = 10, max = 1000, message = "Digite no mínimo 10 e no máximo 1000 caracteres")
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public LocalDateTime getData() {
+		return data;
+	}
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	
+	
+	
+}
